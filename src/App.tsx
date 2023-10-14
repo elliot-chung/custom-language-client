04dc8544bbe0ab9fb3277bb7836c9652bc000b81
Elliot <ec2018@gmail.com>
2023-10-14 02:47:26 -0700

    Linked up with backend

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import axios from 'axios'
 
 function App() {
   const [invalid, setInvalid] = useState(true)
+  const [output, setOutput] = useState("")
+  const [error, setError] = useState(false)
 
   const [memory, setMemory] = useState(-1)
   const [input, setInput] = useState("")
@@ -27,19 +29,24 @@ function App() {
 
   async function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
     e.preventDefault()
-    let res = await axios.post('http://localhost:8080/compile', {
-      memory: memory,
-      input: input,
-      code: code
-    })
-    console.log(res)
-
+    setError(false)
+    setOutput("loading...")
+    try {
+      let res = await axios.post('http://localhost:8080/compile', {
+        memory: memory,
+        input: input,
+        code: code
+      }) 
+      setError(!res.data.success)
+      setOutput(res.data.output)
+    } catch (err) {
+      setError(true)
+      setOutput("Error occurred")
+    }
   }
 
   useEffect(() => {
     if (memory == -1) {
-      setInvalid(true)
-    } else if (input == "") {
       setInvalid(true)
     } else if (code == "") {
       setInvalid(true)
@@ -48,37 +55,34 @@ function App() {
     }
   }, [memory, input, code])
 
-
-
-
-
   return (
     <div className="w-screen h-screen bg-cyan-800 text-white">
       <div className="mx-auto w-3/4 max-w-4xl">
-        <h1 className="uppercase text-3xl font-bold text-center">Custom Language Client</h1>
-        <form className="border rounded-md p-4 shadow" onSubmit={handleSubmit}>
-          <div className="flex my-2">
-            <label htmlFor="memory">Memory:</label>
-            <input className="text-black px-2 ml-4 w-full rounded border" onChange={handleMemChange} type="text" id="memory" name="memory"/>
+        <h1 className="uppercase p-2 text-3xl font-bold text-center">Custom Language Client</h1>
+        <form className="bg-cyan-700 rounded-md p-4 shadow-lg" onSubmit={handleSubmit}>
+          <div className="flex my-2 h-6">
+            <label className="my-auto" htmlFor="memory">Memory:</label>
+            <input className="my-auto text-black px-2 ml-4 w-full rounded border" onChange={handleMemChange} type="text" id="memory" name="memory"/>
           </div>
           {memory == -1 && <p className="text-red-600 text-sm">Invalid Memory</p>}
 
-          <div className="flex my-2">
-            <label htmlFor="input">Input:</label>
-            <input className="text-black px-2 ml-4 w-full rounded border" onChange={handleInputChange} type="text" id="input" name="input" />
+          <div className="flex my-2 h-6">
+            <label className="my-auto" htmlFor="input">Input:</label>
+            <input className="my-auto text-black px-2 ml-4 w-full rounded border" onChange={handleInputChange} type="text" id="input" name="input" />
           </div>
-          {input == "" && <p className="text-red-600 text-sm">Invalid Input</p>}
 
           <label htmlFor="code">Code:</label>
-          <textarea className="text-black font-mono my-2 p-2 h-96 w-full rounded border" onChange={handleCodeChange} id="code" name="code" />
+          <textarea className="whitespace-nowrap text-black font-mono my-2 p-2 h-96 w-full rounded border" onChange={handleCodeChange} id="code" name="code" />
           {code == "" && <p className="text-red-600 text-sm">Invalid Code</p>}
 
           <div className="flex justify-center">
             <button disabled={invalid} className={invalid ? "bg-slate-200 p-2 border rounded" : "hover:bg-cyan-900 p-2 border rounded"} type="submit"> Submit </button>
           </div>
         </form>
-        <div className='font-mono shadow my-2 p-2 border rounded w-full h-96'> 
-          OUTPUT AREA
+        <div className='overflow-auto whitespace-pre font-mono shadow my-2 p-2 bg-cyan-700 rounded-md w-full h-96'> 
+          <div className={error ? "text-red-600" : "text-white"}>
+            <p className="w-fit">{output}</p>
+          </div>
         </div>
       </div>
     </div>
